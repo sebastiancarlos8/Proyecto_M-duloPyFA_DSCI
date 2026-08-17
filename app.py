9b63417b6f9a29e59fb9aa4a190bd4faadce5e56
sebastiancarlos8 <davsebca@gmail.com>
2026-08-16 22:49:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import numpy as np
 
+st.sidebar.image("DMC.png", width = 100)
 st.sidebar.title("Contenido")
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
 if modulos == "Home":
-  st.title("Bienvenido Trabajo Práctico Módulo Python Fundamentals")
+  st.title("Trabajo Práctico - Módulo Python Fundamentals")
   st.image("Python_logo.png", width = 300)
-  st.sidebar.image("DMC.png", width = 100)
-  st.subheader("Elaborado por:")
-  st.write("David Sebastian Carlos Ipanaque")
-  st.subheader("Módulo:")
-  st.write("Especialización en Python for Analytics")
-  st.subheader("Información general:")
+  st.subheader("Elaborado por: David Sebastian Carlos Ipanaque")
+  st.subheader("Módulo: Especialización en Python for Analytics")
+  st.subheader("Información general")
   st.write("Información general: Egresado de la carrera de Ingeniería Industrial, con experiencia en análitica de datos dentro del sector retail, consumo masivo y seguros, dentro del área comercial y de recursos humanos")
   st.markdown("2026")
 
