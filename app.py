084fdd5aeb456c70ab56dbbbd86b7f500c3dfea4
sebastiancarlos8 <davsebca@gmail.com>
2026-08-16 22:34:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ st.sidebar.image("DMC.png", width = 100)
 
 modulos = st.sidebar.selectbox("Seleccione un módulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
-if modulos == "Módulo Listas":
+if modulos == "Home":
   st.write("Bienvenido al módulo de Home")
 
-elif modulos == "Módulo Arreglos":
+elif modulos == "Ejercicio 1":
   st.write("Bienvenido al módulo de Ejercicio 1")
 
-elif modulos == "Módulo Arreglos":
+elif modulos == "Ejercicio 2":
   st.write("Bienvenido al módulo de Ejercicio 2")
 
-elif modulos == "Módulo Arreglos":
+elif modulos == "Ejercicio 3":
   st.write("Bienvenido al módulo de Ejercicio 3")
 
 else:
