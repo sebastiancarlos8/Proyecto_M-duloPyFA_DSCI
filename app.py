3586c6052e54d3d9ad74ba895d2c6a0640655124
sebastiancarlos8 <davsebca@gmail.com>
2026-08-16 22:51:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if modulos == "Home":
   st.title("Trabajo Práctico - Módulo Python Fundamentals")
   st.image("Python_logo.png", width = 300)
   st.subheader("Elaborado por:")
-  st.write.("David Sebastian Carlos Ipanaque")
+  st.write("David Sebastian Carlos Ipanaque")
   st.subheader("Módulo: Especialización en Python for Analytics")
   st.subheader("Información general")
   st.write("Información general: Egresado de la carrera de Ingeniería Industrial, con experiencia en análitica de datos dentro del sector retail, consumo masivo y seguros, dentro del área comercial y de recursos humanos")
