8b317ad55fb90ae6eccda7a2b7da10da7d455794
sebastiancarlos8 <davsebca@gmail.com>
2026-08-16 22:26:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 import streamlit as st
+import numpy as np
+import libreria_funciones as lf 
+
+st.title("Especialización Python for Analytics")
+st.sidebar.title("Parámetros")
+st.write("Elaborado por: Sebastian Carlos")
+
+st.image("Python_logo.png", width = 300)
+st.sidebar.image("DMC.png", width = 100)
+
+modulos = st.sidebar.selectbox("Seleccione un módulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+
+if modulos == "Módulo Listas":
+  st.write("Bienvenido al módulo de Home")
+
+elif modulos == "Módulo Arreglos":
+  st.write("Bienvenido al módulo de Ejercicio 1")
+
+elif modulos == "Módulo Arreglos":
+  st.write("Bienvenido al módulo de Ejercicio 2")
+
+elif modulos == "Módulo Arreglos":
+  st.write("Bienvenido al módulo de Ejercicio 3")
+
+else:
+ st.write("Bienvenido al módulo de Ejercicio 4")
+  
