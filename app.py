1d352b53889d0c696db4182f67078d912c6463f3
sebastiancarlos8 <davsebca@gmail.com>
2026-08-21 23:53:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import librería_funciones_proyecto1 as lf 
+import libreria_funciones_proyecto1 as lf 
 
 st.sidebar.image("DMC.png", width = 100)
 st.sidebar.title("Contenido")
